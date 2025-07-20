8002d3781ccd0deb3e2ce379804779baefbbfdb0
cloner1998 <sajjadyami77@gmail.com>
2025-07-20 23:10:32 +0330

    LongestConsecutive

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import array_and_hashing.Anagram;
 import array_and_hashing.FrequentElements;
+import array_and_hashing.LongestConsecutive;
 import array_and_hashing.ProductOfArray;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {-1, 1, 0 , -3, 3};
-        System.out.println(Arrays.toString(ProductOfArray.productExceptSelf(nums)));
-        System.out.println(ProductOfArray.calculatePrefix(2, nums));
-        System.out.println(ProductOfArray.calculateSuffix(2, nums));
+        int[] nums = {9,1,4,7,3,-1,0,5,8,-1,6,9,9,9,9};
+        System.out.println(LongestConsecutive.longestConsecutive(nums));
+
+        int[] nums2 = {0,3,7,2,5,8,4,6,0,1};
+        System.out.println(LongestConsecutive.longestConsecutive(nums2));
+        int[] nums3 = {0,0,-1};
+        System.out.println(LongestConsecutive.longestConsecutive(nums3));
     }
 }
