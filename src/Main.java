e778e79ce89d7205cdfe84e9b32049297790a523
cloner1998 <sajjadyami77@gmail.com>
2025-07-21 22:29:28 +0330

    three sum better performance

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import two_pointers.ThreeSum;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
-        System.out.println(ThreeSum.threeSum_slow(nums));
         System.out.println(ThreeSum.threeSum(nums));
     }
 }
