433791fbf37df1894fc9bc2a2e66580bc8ce00b0
cloner1998 <sajjadyami77@gmail.com>
2025-07-23 23:03:57 +0330

    LongestSubstringWithoutRepeatingCharacters O(n) and O(n^2) version

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,40 @@
-public class LongestSubstringWithoutRepeatingCharacters {
-    public static int lengthOfLongestSubstring(String s) {
+import java.util.*;
 
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    // this is O(n^2)
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        int max = 1;
+        Set<Character> set = new HashSet<>();
+        char[] chars = s.toCharArray();
+        int start = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (set.contains(chars[i])) {
+                set.remove(chars[start]);
+                start++;
+            }
+
+            set.add(chars[i]);
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+
+    // this is O(n) version
+    public static int lengthOfLongestSubstring2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0, start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                start = Math.max(start, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import sliding_window.BestTimeToBuyAndSellStock;
 
 public class Main {
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
-        System.out.println(BestTimeToBuyAndSellStock.maxProfit(prices));
+        String s = "dvdf";
+        System.out.println(LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring(s));
     }
 }
