368ebd67447a7bb93c46147cccc739d2ba87f498
cloner1998 <sajjadyami77@gmail.com>
2025-07-24 03:41:25 +0330

    SmallestSubArray fix

--- src/sliding_window/SmallestSubArray.java
+++ src/sliding_window/SmallestSubArray.java
@@ -2,8 +2,8 @@ package sliding_window;
 
 public class SmallestSubArray {
     public static int smallestSubArray(int[] nums, int target) {
-        int smallestLength = 0;
-        int sum = Integer.MIN_VALUE;
+        int smallestLength = Integer.MAX_VALUE;
+        int sum = 0;
         int left = 0;
         for (int i = 0; i < nums.length; i++) {
             sum = sum + nums[i];
